b91456ef8b14bc5113dfeb393a6afb43e338efa2
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-30 23:32:31 +0530

    Connection file  created

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+   $host = "localhost";
+   $user = "Aditya66";
+   $pass = "Github@123456";
+   $db = "id13776182_blooddb";
+   
+   $con = mysqli_connect($host, $user, $pass, $db);
+   
+   
+   if($con)
+   {
+       echo "Connected to Database";
+   }else{
+       echo "Failed to connect".mysqli_connect_error();
+   }
+?>
